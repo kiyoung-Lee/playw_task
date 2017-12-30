0124b345c220a5cda47d6507cf28d8e20d095ea6
KiyoungLee <eevee300@gmail.com>
2017-12-31 00:48:49 +0900

    아이콘 추가 / 데이터 바인딩 진행중

--- app/src/main/java/com/example/playwingstask/Main/MainActivity.java
+++ app/src/main/java/com/example/playwingstask/Main/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.playwingstask.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.playwingstask.Main.Data.MainRepositoryImpl;
+import com.example.playwingstask.Main.Data.Model.DetailPage;
 import com.example.playwingstask.R;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements MainContract.activity {
+
+    @BindView(R.id.main_image)
+    ImageView mainImageView;
+    @BindView(R.id.main_info)
+    TextView mainInfo;
+    @BindView(R.id.main_title)
+    TextView title;
+    @BindView(R.id.main_arrival_date)
+    TextView arrivalDate;
+    @BindView(R.id.main_caution)
+    TextView caution;
+    @BindView(R.id.like_count)
+    TextView likeCount;
+    @BindView(R.id.reply_count)
+    TextView replyCount;
+    @BindView(R.id.city_name)
+    TextView cityName;
+    @BindView(R.id.trip_price)
+    TextView tripPrice;
+    @BindView(R.id.document_list)
+    RecyclerView documentList;
 
     private MainContract.presenter presenter;
     private MainAdapter adapter;
@@ -19,11 +46,32 @@ public class MainActivity extends AppCompatActivity implements MainContract.acti
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         adapter = new MainAdapter();
-
+        documentList.setLayoutManager(new LinearLayoutManager(this));
+        documentList.setAdapter(adapter);
 
         presenter = new MainPresenterImpl(new MainRepositoryImpl());
+        presenter.setActivityView(this);
+        presenter.setDocumentAdapterModel(adapter);
+        presenter.setDocumentAdapterView(adapter);
         presenter.start();
+    }
+
+    @Override
+    public void showTripDetailView(DetailPage detailPageData) {
+        title.setText(detailPageData.getTitle());
+        mainInfo.setText(detailPageData.getInfo());
+        Glide.with(this).load(detailPageData.getImageUrl()).into(mainImageView);
+        likeCount.setText(String.valueOf(detailPageData.getLikecount()));
+        replyCount.setText(String.valueOf(detailPageData.getCommentCount()));
+        cityName.setText(detailPageData.getMainCityName());
+        tripPrice.setText(String.valueOf(detailPageData.getMainPrice()));
+    }
+
+    @Override
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/playwingstask/Main/MainDocumentViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.playwingstask.Main;
+
+import android.content.Context;
+import android.view.View;
+
+import com.example.playwingstask.Common.BaseRecyclerViewHolder;
+import com.example.playwingstask.Main.Data.Model.Document;
+
+/**
+ * Created by KiyoungLee on 2017-12-30.
+ */
+
+public class MainDocumentViewHolder extends BaseRecyclerViewHolder<Document, MainContract.presenter>{
+
+    public MainDocumentViewHolder(Context context, View itemView) {
+        super(context, itemView);
+    }
+
+}
